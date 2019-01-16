5e8fc35a37588c6107b57d13c98ce3ca2d179dcc
Mario Fridrichovsky <mario.fridrichovsky@wksimonsfeld.at>
2019-01-16 07:40:31 +0100

    added electron-pug

--- app.js
+++ app.js
@@ -4,39 +4,46 @@ const config = new ElectronConfig();
 const url = require("url");
 const path = require("path");
 const dt = require("directory-tree");
+const pug = require("electron-pug");
 
 let mainWindow;
 
 electron.app.on("ready", () => {
     setTimeout(() => {
-        const woptions = {
-            show: false,
-            frame: false,
-            width: 800,
-            height: 600,
-            transparent: true,
-            resizeable: true
-        };
-        Object.assign(woptions, config.get("window"));
+        pug({ pretty: true })
+        .then(pugData => {
+            console.log(pugData);
 
-        mainWindow = new electron.BrowserWindow(woptions);
-        mainWindow.loadURL(url.format({
-            pathname: path.join(__dirname, "html", "index.html"),
-            protocol: "file:",
-            slashes: true
-        }));
+            const woptions = {
+                show: false,
+                frame: false,
+                width: 800,
+                height: 600,
+                transparent: true,
+                resizeable: true
+            };
+            Object.assign(woptions, config.get("window"));
 
-        mainWindow.on("close", () => {
-            config.set("window", mainWindow.getBounds());
-        });
+            mainWindow = new electron.BrowserWindow(woptions);
+            mainWindow.loadURL(url.format({
+                pathname: path.join(__dirname, "html", "index.html"),
+                protocol: "file:",
+                slashes: true
+            }));
 
-        mainWindow.on("closed", () => {
-            electron.app.quit();
-        });
+            mainWindow.on("close", () => {
+                config.set("window", mainWindow.getBounds());
+            });
 
-        mainWindow.once("ready-to-show", () => {
-            mainWindow.show();
-        });
+            mainWindow.on("closed", () => {
+                electron.app.quit();
+            });
+
+            mainWindow.once("ready-to-show", () => {
+                mainWindow.show();
+            });
+        })
+        .catch(console.error);
     }, 50);
 });
 
